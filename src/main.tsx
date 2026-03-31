2578e33346ca2b0478d005d013bb78f0e1e94bac
vkotelnykovaa <lerkotelnikova6@gmail.com>
2026-03-31 12:05:14 +0300

    Project is ready3.0

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import App from '../src/App/App';
+import App from './components/App/App';
 
 const queryClient = new QueryClient();
 
